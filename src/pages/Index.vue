c71c68ae3420ef20c71bd3dc04e2f91afba35c8c
Mayk Jony <drum.ayk@gmail.com>
2020-02-25 00:36:52 +0300

    add more infoJ

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,15 +6,35 @@
 
       <h1 class="font-bold text-3xl mt-4">Patients</h1>
 
-      <ol class="my-4" v-for="page in $page.allGoogleSheet.edges" :key="page.node.id">
-        <li v-if="page.node.First_Name">
+      <ol class="list-decimal pl-6">
+        <li class="my-4" v-for="page in $page.allGoogleSheet.edges" :key="page.node.id">
           <g-link class="capitalize" :to="`/sheet/${page.node.id}`">
-            <strong class="capitalize" v-if="page.node.Last_Name">{{ page.node.Last_Name}}, </strong>{{ page.node.First_Name }}  -
+            <strong class="capitalize" v-if="page.node.Last_Name">{{ page.node.Last_Name}},</strong>
+            {{ page.node.First_Name }} -
             <i class="italic capitalize">({{ page.node.Nationality }})</i>
             <!-- - {{ page.node.id }} -->
           </g-link>
         </li>
       </ol>
+
+      <!-- <div class="mt-4">
+        <div
+          class="max-w-sm mb-2 mx-auto flex p-4 bg-white rounded-lg shadow-md"
+          v-for="(item, index) in $page.allFaker.edges"
+          :key="index"
+        >
+          <div class="flex-shrink-0">
+            <g-image class="h-16 w-16 rounded-full" :src="item.node.avatar" alt="ChitChat Logo" />
+          </div>
+          <div class="ml-4 pt-3">
+            <h4 class="text-base text-gray-900 leading-tight">{{ item.node.author }}</h4>
+            <p class="text-sm text-gray-600 leading-normals">{{ item.node.email }}</p>
+          </div>
+        </div>
+      </div> -->
+
+
+
     </div>
   </Layout>
 </template>
--- src/templates/googleSheet.vue
+++ src/templates/googleSheet.vue
@@ -2,16 +2,26 @@
   <Layout>
     <div class="container">
       <p class="mt-4 text-gray-700 text-sm">Patient Name</p>
-      <h1 class="font-bold text-3xl">
+      <h1 class="font-bold text-3xl leading-8">
         <span class="capitalize" v-if="$page.googleSheet.Last_Name">{{ $page.googleSheet.Last_Name}}, </span>
         {{ $page.googleSheet.First_Name }} 
       </h1>
-      <h2 class="mt-2"><strong>Id: </strong> {{ $page.googleSheet.id }}</h2>
+      <p class="mt-4"><strong>Id: </strong> {{ $page.googleSheet.id }}</p>
+      <p class=""><strong>Year of Birth:</strong> {{ $page.googleSheet.Year_of_Birth }}</p>
+      <p><strong>Grafts:</strong> {{ $page.googleSheet.Grafts }}</p>
+      <p class="capitalize"><strong>Nationality:</strong> {{ $page.googleSheet.Nationality }}</p>
+      <p><strong>Mail:</strong> {{ $page.googleSheet.Mail }}</p>
+      <p><strong>Phone:</strong> {{ $page.googleSheet.Phone }}</p>
+      <p><strong>Occupation:</strong> {{ $page.googleSheet.Occupation }}</p>
+      <p><strong>Surgery_Date:</strong> {{ $page.googleSheet.Surgery_Date }}</p>
+
 
       <div class="mt-4">
-        <p>
-          <strong>{{ $page.googleSheet.First_Name }}</strong>
+        <p class="font-bold text-2xl">Notes</p>
+        <p v-if="$page.googleSheet.Notes">
+          {{ $page.googleSheet.Notes }}
         </p>
+        <p class="text-gray-700" v-else>There is no not about this patient.</p>
         <!-- <p>{{ $page.googleSheet.organizer }}</p> -->
       </div>
     </div>
@@ -23,7 +33,14 @@ query GoogleSheet ($path: String!) {
   googleSheet (path: $path) {
     First_Name 
     Last_Name
+    Occupation
+    Year_of_Birth
+    Surgery_Date
+    Nationality
     id 
+    Grafts
+    Mail
+    Phone
     }
 }
 </page-query>
